0c8fc9039344c3325ed788a986def98519335692
tak758 <164702872+tak758@users.noreply.github.com>
2024-04-25 21:05:35 +0900

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((objc-mode . ((tab-width . 4) (indent-tabs-mode . t))))
